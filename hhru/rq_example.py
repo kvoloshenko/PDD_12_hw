e1fd898d6e0b0b35691e9cdfd0c566af71286d1e
kvoloshenko <kvoloshenko@gmail.com>
2024-09-28 14:24:06 +0300

    Implemented for hh.ru

--- hhru/rq_example.py
+++ hhru/rq_example.py
@@ -1,9 +1,13 @@
-import  requests
-import pprint
+# Этот модуль предназначен для поиска вакансий на сайте HeadHunter с помощью API.
+# Этот модуль позволяет быстро находить вакансии по заданным критериям и получать подробную информацию
+# о конкретных вакансиях, используя API HeadHunter.
 
-DOMAIN = 'https://api.hh.ru/'
+import  requests # Импортируем библиотеку для выполнения HTTP-запросов
+import pprint    # Импортируем библиотеку для красивого вывода данных
 
-url_vacancies = f'{DOMAIN}vacancies'
+DOMAIN = 'https://api.hh.ru/' # Доменный адрес API HeadHunter
+
+url_vacancies = f'{DOMAIN}vacancies' # URL для получения списка вакансий
 
 # params = {'text': 'Python developer',
 #           'page': 1}
@@ -12,23 +16,30 @@ url_vacancies = f'{DOMAIN}vacancies'
 # params = {'text': 'NAME:(Python OR AI) and (Django OR Keras)',
 #           'page': 1}
 
+# Параметры запроса для поиска вакансий
 params = {'text': 'NAME:(Python) and (AI OR ML OR Keras OR Numpy OR Pandas)',
           'page': 1}
 
-
+# Выполняем GET-запрос к API для получения списка вакансий с указанными параметрами
 result =  requests.get(url_vacancies, params = params).json()
 
 # print(result.status_code)
 # pprint.pprint(result)
 
+# Извлекаем список вакансий из результата запроса
 items = result['items']
+
+# Извлекаем первую вакансию из списка
 first = items[0]
 # pprint.pprint(first)
 
+# Печатаем URL страницы с подробной информацией о первой вакансии
 print(first['alternate_url'])
 print(first['url'])
 
+# URL для получения полной информации о первой вакансии
 one_vacancy_url = first['url']
 
+# Выполняем GET-запрос к API для получения полной информации о первой вакансии
 result =  requests.get(one_vacancy_url, params = params).json()
 pprint.pprint(result)
